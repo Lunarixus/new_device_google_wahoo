d9b3b2019dc3ac2a593d95032542cd1a813abf35
Lunarixus <Lunarixus@raphielgang.org>
2020-01-27 02:01:24 +0000

    wahoo: Enable EUICC features
    Thanks to raymanfx
    
    Signed-off-by: Lunarixus <Lunarixus@raphielgang.org>

--- device.mk
+++ device.mk
@@ -726,3 +726,7 @@ PRODUCT_PACKAGES += \
    com.android.ims.rcsmanager \
    RcsService \
    PresencePolling
+
+# EUICC feature
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.telephony.euicc.xml
